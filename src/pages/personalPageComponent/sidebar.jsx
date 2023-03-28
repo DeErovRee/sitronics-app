108397f1b00b3e6a8850cfcd23f81f02bd4bc0cf
Denis Nesterov <den_nesterov_1992@mail.ru>
2023-03-28 13:25:35 +0500

    Start working on user settings in personal area

--- src/pages/personalPageComponent/sidebar.jsx
+++ src/pages/personalPageComponent/sidebar.jsx
@@ -1,9 +1,9 @@
 import React, { useContext, useEffect } from "react"
 import { auth, db } from "../../firebase/firebase"
 import { AuthContext } from "../../context/AuthContext"
-import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { doc, onSnapshot } from "firebase/firestore";
 
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useState } from "react";
 
 export const Sidebar = () => {
@@ -11,33 +11,34 @@ export const Sidebar = () => {
     const [ provider, setProvider ] = useState()
 
     useEffect(() => {
-        const unsub = onSnapshot(doc(db, "users", currentUser.uid), (doc) => {
+        const unsub =  onSnapshot(doc(db, "users", currentUser.uid), (doc) => {
           doc.exists() && setProvider(doc.data().isProvider);
         });
     
         return()=>{
           unsub()
         }
-      }, [currentUser]);
+      }, []);
 
     const logout = async () => {
         try {
           await auth.signOut();
         } catch (error) {
         }
-      };
+    };
 
-    console.log(provider)
     return(
         <div className="personalSidebar">
             <img src={currentUser.photoURL} alt="" width='65px' className="userPhoto" />
             <p className="userName">{currentUser.displayName}</p>
-            <p className="userEmail">{currentUser.email}</p>
+            <p className="userEmail" onClick={()=> window.location = `mailto:${currentUser.email}`}>{currentUser.email}</p>
+          
             {provider ? <p className="userStatus provider">Поставщик услуг</p> : <p className="userStatus client">Клиент</p>}
             <Link to="ordersAll">Заявки</Link>
             <Link to="orders">Текущие заказы</Link>
             <Link to="ordersHistory">История заказов</Link>
             <Link to="settings">Настроить профиль</Link>
+           
             {provider && <Link to="providerButton">Кнопка поставщика</Link>}
             <button onClick={logout}>Выйти</button>
         </div>
